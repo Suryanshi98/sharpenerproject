b44b9564e9675a6e62d203a56c712c27407773cf
suryanshi98 <suryawanshilokesh3@gmail.com>
2023-04-03 11:02:14 +0530

    QuerySelector

---
+++ queryselector.js
@@ -0,0 +1,19 @@
+
+//queryselector//
+var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.backgroundcolor= 'green';
+
+//queryselectorall
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var secondItem = doument.querySelectorAll('li:nth-child(2');
+
+
+
+for(var i = 0; i < items.length; i++){
+    odd[i].style.backgroundcolor = 'green';
+    secondItem.style.color = 'green';
+}
+
+
+
